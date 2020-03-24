bade96f6cb79c64d0531783a7aff8b83c0a6f0ee
root <root@48b4be17441f>
2020-03-24 18:30:33 +0000

    Finished the runner class

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,19 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame j;
+	final int WIDTH = 500;
+	final int HEIGHT = 800;
+public static void main(String[] args) {
+	LeagueInvaders game = new LeagueInvaders();
+	game.setup();
+}
+public LeagueInvaders() {
+	 j = new JFrame();
+}
+void setup() {
+	j.setSize(WIDTH, HEIGHT);
+	j.setVisible(true);
+	j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 }
