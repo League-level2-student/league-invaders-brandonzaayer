8fdae18e68d02c4201bf3b7dea04ed3375661683
ExhibitView User <ExhibitView User@192.168.86.51>
2020-03-27 11:28:12 -0700

    Step 5 of alien and projectile spawn

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,7 +5,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
@@ -14,12 +16,19 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
     final int END = 2;
     int currentState = MENU;
     Font titleFont;
-    Rocketship r;
+    Rocketship r = new Rocketship(200, 500, 50, 50);
+    ObjectManager object = new ObjectManager(r);
+    public static BufferedImage image;
+    public static boolean needImage = true;
+    public static boolean gotImage = false;	
+    Timer alienSpawn;
     public GamePanel() {
     	titleFont  = new Font("Arial", Font.PLAIN, 48);
     	Timer frameDraw = new Timer(1000/60,this);
     	frameDraw.start();
-    	r = new Rocketship(200, 500, 50, 50);
+    	if (needImage) {
+    	    loadImage ("space.png");
+    	}
     }
 	@Override
 	public void paintComponent(Graphics g){
@@ -33,11 +42,22 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		    drawEndState(g);
 		}
 	}
+	void loadImage(String imageFile) {
+	    if (needImage) {
+	        try {
+	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+		    gotImage = true;
+	        } catch (Exception e) {
+	            
+	        }
+	        needImage = false;
+	    }
+	}
 	void updateMenuState() {
 		
 	}
 	void updateGameState() {
-		
+		object.update();
 	}
 	void updateEndState() {
 		
@@ -58,9 +78,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		g.drawString("Press SPACE for instructions", 75, 400);
 	}
 	void drawGameState(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-		r.draw(g);
+		if (gotImage) {
+			g.drawImage(image, 0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT, null);
+		} else {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		}
+		object.draw(g);
 	}
 	void drawEndState(Graphics g) {
 		g.setColor(Color.RED);
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,13 +1,16 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class ObjectManager {
+public class ObjectManager implements ActionListener{
 Rocketship rocket;
 ArrayList<Projectile> projectiles = new ArrayList<Projectile>(); 
 ArrayList<Alien> aliens = new ArrayList<Alien>(); 
 Random random = new Random();
-public ObjectManager(Rocketship rock) {
-	rocket = rock;
+public ObjectManager(Rocketship ship) {
+	this.rocket = ship;
 }
 void addProjectile(Projectile p) {
 	projectiles.add(p);
@@ -18,7 +21,36 @@ void addAlien() {
 void update() {
 	for(int i = 0; i < aliens.size(); i++){
 		aliens.get(i).update();
-		
 	}
+	for(int i = 0; i < projectiles.size(); i++){
+		projectiles.get(i).update();
+	}
+}
+void draw(Graphics g) {
+	rocket.draw(g);
+	for(int i = 0; i < aliens.size(); i++){
+		aliens.get(i).draw(g);
+	}
+	for(int i = 0; i < projectiles.size(); i++){
+		projectiles.get(i).draw(g);
+	
+}
+}
+void purgeObjects() {
+	for(int i = aliens.size(); i > 0; i--){
+		if(aliens.get(i).isActive == false) {
+			aliens.remove(i);
+		}
+	}
+	for(int i =  projectiles.size(); i > 0; i--){
+		if(projectiles.get(i).isActive == false) {
+			projectiles.remove(i);
+		}
+}
+}
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	addAlien();
 }
 }
